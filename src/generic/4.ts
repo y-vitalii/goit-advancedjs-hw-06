c7ce379dcd0ec397dc7ebe41397e15255b69daf1
y-vitalii <vitalii.yermokhin@gmail.com>
2026-06-27 12:28:01 +0200

    goit-advancedjs-hw-06

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,13 +2,17 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
+interface HasTitle {
+  title: string;
+}
+
+class Component<T> {
+  constructor (public props: T) {
 
   }
 }
 
-class Page extends Component {
+class Page extends Component<HasTitle> {
   pageInfo () {
     console.log(this.props.title);
   }
